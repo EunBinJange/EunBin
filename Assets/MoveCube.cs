f96268849b9d1e35426aa2fd0bf7b7206ffb6781
EunBinJang <jang020410@naver.com>
2022-04-18 16:02:29 +0900

    Lec_7 오브젝트 이동 스크립트

--- Assets/MoveCube.cs
+++ Assets/MoveCube.cs
@@ -11,7 +11,7 @@ public class MoveCube : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+        //Debug.Log("Hello World");
         startPos = transform.position;
     }
     
